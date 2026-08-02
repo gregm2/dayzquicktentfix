7482ad3fc2e810881f27ced7ea2867cece910c59
gregm <moorer.greg@gmail.com>
2026-08-01 21:51:09 -0400

    fix
    this might help

--- QuickTentFix/scripts/4_world/tentbasefix.c
+++ QuickTentFix/scripts/4_world/tentbasefix.c
@@ -2,7 +2,8 @@ modded class TentBase
 {
 	override bool OnStoreLoad(ParamsReadContext ctx, int version)
 	{
-		super.OnStoreLoad(ctx, version);
-		m_OpeningMask = 0; /* should cause all flaps/windows/doors to be open on load
+		bool result = super.OnStoreLoad(ctx, version);
+		m_OpeningMask = 0; // should cause all flaps/windows/doors to be open on load
+		return result;
 	}
 };
